09001a6e218a76dc0b820a34adf6badba87a6d87
Beau Cranston <beau2tie@gmail.com>
2021-03-01 00:22:47 -0500

    fgdg

--- js/main.js
+++ js/main.js
@@ -20,14 +20,19 @@ $("#about-me-link").click((event)=>{$('.main').moveTo(2)})
 $("#my-works-link").click((event)=>{$('.main').moveTo(3)})
 $("#contact-link").click((event)=>{$('.main').moveTo(4)})
 
-function characterSpriteAnimation(sprite, animation, src, callback){
+function characterSpriteAnimation(sprite, animation, src, end, callback){
     var sprite = document.getElementById(sprite);
     var spriteContainer = sprite.parentElement;
+    if(spriteContainer.classList.contains('d-none')){
+        spriteContainer.classList.remove('d-none')
+    }
     spriteContainer.classList.add(animation);
     sprite.classList.add('spritesheet');
     sprite.src = src;
     spriteContainer.addEventListener('animationend', (event)=>{
-        spriteContainer.style.display='none'
+        if(end){
+            spriteContainer.style.display='none'
+        }
         console.log('sprite animation ended');
         if(callback){
             callback();
@@ -50,7 +55,7 @@ function playHeaderAnimation() {
     triangle.addEventListener('animationend', ()=>{
         triangle.style.display = 'none';
         var avatar = document.querySelector('.avatar'); 
-        characterSpriteAnimation('header-sprite', 'header-sprite-animation', './images/Pixel Art/me-run2-Sheet.png')
+        characterSpriteAnimation('header-sprite', 'header-sprite-animation', './images/Pixel Art/me-run2-Sheet.png', true)
         avatar.classList.remove('spin-y');
         avatar.classList.add('roll-away');
         avatar.addEventListener('animationend', ()=>{
@@ -58,7 +63,7 @@ function playHeaderAnimation() {
             console.log('avataranimation ended');
             var subtitle = document.getElementById('subtitle');
             var subtitleContainer = subtitle.parentElement.parentElement;
-            subtitleContainer.classList.remove('d-none');
+            subtitleContainer.classList.remove('d-md-none');
             subtitleContainer.classList.add('fade-in');
     
         });
@@ -66,5 +71,41 @@ function playHeaderAnimation() {
 }
 
 function playAboutMeAnimation(){
-    characterSpriteAnimation('about-me-sprite', 'about-me-animation', './images/Pixel Art/me-run2-Sheet.png');
+    characterSpriteAnimation('about-me-sprite', 'about-me-right-fall', './images/Pixel Art/me-fall-Sheet.png', false, ()=>{
+        showInfoBox('info-box-right', 'top-border-right');
+        characterSpriteAnimation('about-me-sprite', 'about-me-right-run', './images/Pixel Art/me-run2-Sheet.png', false, ()=>{
+            characterSpriteAnimation('about-me-sprite', 'about-me-right-jump', './images/Pixel Art/me-jump-Sheet.png', false, ()=>{
+                characterSpriteAnimation('about-me-sprite', 'about-me-center-fall', './images/Pixel Art/me-fall-Sheet.png', false, ()=>{
+                    showInfoBox('info-box-center', 'top-border-center');
+                    fadeIn('line-1')
+                    characterSpriteAnimation('about-me-sprite', 'about-me-center-run', './images/Pixel Art/me-run2-Sheet.png', false, ()=>{
+                        characterSpriteAnimation('about-me-sprite', 'about-me-center-jump', './images/Pixel Art/me-jump-Sheet.png', false, ()=>{
+                            characterSpriteAnimation('about-me-sprite', 'about-me-left-fall', './images/Pixel Art/me-fall-Sheet.png', false, ()=>{
+                                showInfoBox('info-box-left', 'top-border-left');
+                                fadeIn('line-2')
+                                characterSpriteAnimation('about-me-sprite', 'about-me-left-run', './images/Pixel Art/me-run2-Sheet.png', true)
+                            });
+                        });
+                        
+                    });
+                });
+            });
+        });
+              
+    });
+    
+}
+
+
+function showInfoBox(infoBox, topBorder){
+    var border = document.getElementById(topBorder)
+    fadeIn(infoBox)  
+    border.classList.add('d-none');
+}
+function fadeIn(element){
+    var e = document.getElementById(element);
+    e.classList.add('fade-in');
+    if(e.classList.contains('d-none')){
+        e.classList.remove('d-none');
+    }
 }
